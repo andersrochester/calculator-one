d4031b7862ab9e269ce76911e06e18cb4cbaf577
andersrochester <anders.rochester@gmail.com>
2017-09-12 13:21:49 +0200

    skipped string, working with array instead

--- script.js
+++ script.js
@@ -13,38 +13,32 @@ Has one stack.
 $(document).ready(function(){
 // validering och klippa av strängen på rätt ställe vid input är inte helt klar
 // får kanske tänka helt nytt.....
-	var testNumLength = function(number) {
-        if (number.length < 9) {
-            totaldiv.css({"font-size" : "60px"});
-        };
+// Tar och använder mig av en array istället.
 
-        if (number.length > 9) {
-            totaldiv.css({"font-size" : "39px"});
-            number =  number.slice(0,14);
-        };
-        if (number.length > 20) {
-            number = "";
-            totaldiv.text("Err");
-        };
-    };
-
-    var number = "";
-    var newnumber = "";
-    var result;    
+    var numberArray = [];
+    var i;
+    var number = 0;
+    var newnumber = 0; 
+    var result = 0;    
     var operator = "";
     var testComma = false; 
     var totaldiv = $("#total");
     totaldiv.text("0");
-    $("#numbers a").not("#clear,#clearall, #comma").click(function(){
-		number += $(this).text();
-		totaldiv.text(number);
-		testNumLength(number);
+
+    $("#numbers a").not("#equals, #comma").click(function(){
+  	   if(numberArray.length < 12)
+            numberArray.push( Number($(this).text()) );
+        i++;
+        // present the array in calculators window
+  		totaldiv.text(numberArray.join(""));
+            number = Number(numberArray.join(""));
     });
 
     // comma test 
     $("#comma").click(function(){
         if (testComma === false ) {
             testComma = true;
+    		numberArray.push( $(this).text() );
     		number += $(this).text();
         }
     });
@@ -53,26 +47,24 @@ $(document).ready(function(){
         operator = $(this).text();
         testComma = false;
 		newnumber = number;
-		number = "";
+		number = 0; 
+        numberArray = [];
 		totaldiv.text("0");
     });
 
     $("#clear,#clearall").click(function(){
-        totaldiv.css({"font-size" : "60px"});
-		number = "";
+        i = 0;
+        numberArray = [];
+		number = 0;
 		totaldiv.text("0");
         testComma = false;
 		if ($(this).attr("id") === "clearall") {
-		newnumber = "";
+		newnumber = 0;
 		}
     });
 
     $("#equals").click(function(){
         testComma = false;
-        // convert the strings to number
-    	testNumLength(number);
-        number = parseFloat(number);
-        newnumber = parseFloat(newnumber);
     	if (operator === "+"){
     	    result = (number + newnumber);
     	} else if (operator === "-"){
@@ -82,28 +74,15 @@ $(document).ready(function(){
     	} else if (operator === "*"){
             result = (newnumber * number);
     	}
-
-        console.log(result);
-        // handle very big numbers and very small numbers
-        if (result > 100000000 || result < 0.000001) {
-            result = result.toExponential(6);
+// en del stora tal går utanför skärmen
+// skall arbeta med en annan validering av längden  på strängen            
+        if(result > 10000000 || result < 0.0000001){
+    	    totaldiv.text(result.toExponential(6));
+        } else {
+    	    totaldiv.text(result.toPrecision(9));
         }
-
-        // convert number to string
-        result = result.toString(10);
-        // test result text string
-        testNumLength(result);
-        console.log(result);
-        // present text string in DOM
-    	totaldiv.text(result.toString(10));
             
-        // catch error for errors in calculations
-            if (result !== "Err" || result !== "Infinity") {
-               number = result;
-            } else {
-               number = "";
-            }
         testComma = false;
-    	newnumber = "";
+    	newnumber = 0; 
     });
 });
